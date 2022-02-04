547708c1330becd5c16f212720977197d5bcec2d
AndTorGuer <atorresguerrero9@gmail.com>
2022-02-04 13:21:54 -0500

    Initial commit

---
+++ Lab2_Andres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_Andres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
